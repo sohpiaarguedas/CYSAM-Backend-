7ab69510d29c979902bd3dfd7fd1b876acab2f8f
sohpiaarguedas <arguemora@gmail.com>
2026-06-17 16:03:31 -0600

    Cambios

--- server.ts
+++ server.ts
@@ -13,9 +13,6 @@ app.use('/api', (req, res) =>{
     res.status(404).json({ message: 'Endpoint not found' });
 });
 
-
 app.listen(env.PORT, () => {
     console.log(`Server running on port ${env.PORT}`);
 });
-
-
