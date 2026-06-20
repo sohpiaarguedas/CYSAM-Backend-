2212ffd35d7889007dcf94fccfc982b4c45638b2
RicardoPerezJ <ricardo.perezjimenez@ucr.ac.cr>
2026-06-20 16:17:01 -0600

    Se Modifica para la implementacion del chatbot

--- server.ts
+++ server.ts
@@ -1,14 +1,17 @@
+import 'dotenv/config';
 import app from './src/app';
 import env from './env';
 //import created routes
 
 import articlesRoutes from "./src/routes/articlesRoutes";
+import aiRoutes from './src/routes/aiRoutes';
 
 //use routes
 app.use('/api/articles', articlesRoutes);
+app.use('/api/ai', aiRoutes)
 
 
-app.use('/api', (req, res) =>{
+app.use('/api', (req, res) => {
     res.status(404).json({ message: 'Endpoint not found' });
 });
 
