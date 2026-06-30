3df59d5649aa23ff1ae6af11319394fdacd3c9e0
Esteban Leiton <ejosueleitonferreto@gmail.com>
2026-06-30 01:53:01 -0600

    Se trabajó en el manejo de Inicio de Sesión y Registro mediante el fetch. Se manejaron las rutas respectivas y se modificó el schema para que permitiera guardar el usuario en la base de datos. Se crearon los archivos de authController,passwords para manejar el hash y bcrypt. Se trabajó en: authController,passwords,authRoutes, schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -16,10 +16,12 @@ export const articles = pgTable('articles', {
     summary: text('summary').notNull(),
     media_url: text('media_url').notNull(),
     media_type: text('media_type').notNull(),
-    link_url: text('link_url').notNull(),
-    link_preview: text('link_preview').notNull(),
-    link_Image_Preview: text('linkImagePreview').notNull(),
-    button_text: text('button_text').notNull(),
+    //LAS CAMBIÉ POR ERROR QUE ME DABA AL DARLE EL PUSH A LA BD
+    link_url: text('link_url').default(""),
+    link_preview: text('link_preview').default(""),
+    link_Image_Preview: text('linkImagePreview').default(""),
+    button_text: text('button_text').default(""),
+    //////////////////////////////////////////////////////////
 
     created_at: timestamp('created_at').defaultNow().notNull(),
     updated_at: timestamp('updated_at').defaultNow().notNull()
